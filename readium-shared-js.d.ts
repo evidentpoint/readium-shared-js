a063f2fc72b23ea6d25395e90b21e30fe872cf78
Juan Corona <juan.carlos.corona.r@gmail.com>
2018-08-24 06:36:26 -0700

    Expose IframeLoader

--- readium-shared-js.d.ts
+++ readium-shared-js.d.ts
@@ -3,6 +3,7 @@ export var Globals: any;
 export var ReaderView: any;
 export var ReflowableView: any;
 export var OnePageView: any;
+export var IframeLoader: any;
 export var Package: any;
 export var ViewerSettings: any;
 export var StyleCollection: any;
